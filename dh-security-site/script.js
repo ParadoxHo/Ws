63d25139269bfcf30fad54b56f6b610adb25c085
ParadoxHo <bzpk126@gmail.com>
2026-04-09 19:40:45 +0300

    Add send function to handle WhatsApp queries

---
+++ dh-security-site/script.js
@@ -0,0 +1,9 @@
+function send(){
+let n=document.getElementById('name').value;
+let p=document.getElementById('phone').value;
+let m=document.getElementById('msg').value;
+
+let text=`Zapytanie DH Security %0A ${n} %0A ${p} %0A ${m}`;
+
+window.open(`https://wa.me/48888888888?text=${text}`);
+}
